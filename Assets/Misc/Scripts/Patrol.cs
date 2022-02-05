04d30586be325bef96692c54f846999aa607cc2c
ahmed-alnour123 <ahmed2699@gmail.com>
2022-02-05 11:10:49 +0200

    worked in patrol script again

--- Assets/Misc/Scripts/Patrol.cs
+++ Assets/Misc/Scripts/Patrol.cs
@@ -7,7 +7,20 @@ using UnityEngine;
 /// </summary>
 public class Patrol : MonoBehaviour {
 
+    public float speed;
+    public float threshold;
+
     List<Vector2> points;
+
+    private Vector2 point {
+        get { return points[index % points.Count]; }
+    }
+
+    private Vector2 nextPoint {
+        get { return points[(index + 1) % points.Count]; }
+    }
+
+    private int index = 0;
 
     void Start() {
         foreach (Transform child in transform.Find("patrol")) {
@@ -17,6 +30,10 @@ public class Patrol : MonoBehaviour {
 
 
     void Update() {
-
+        if (Vector2.Distance(transform.position, nextPoint) < threshold) {
+            transform.position = Vector2.MoveTowards(transform.position, nextPoint, speed);
+        } else {
+            index++;
+        }
     }
 }
